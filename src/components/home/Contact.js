2779935fa59c50e400f9f9ba30fc9ec5850e886d
S0f1eM <s.marchand56@gmail.com>
2020-06-24 17:15:32 +0200

    fix missing link in contact form

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 
-const Contact = props => {
+const Contact = () => {
   const email = 'mailto:s.marchand56@gmail.com';
-
+  const linkedin = "https://www.linkedin.com/in/sophie-marchand-web-developer/";
+  const twitter = "https://twitter.com/S0f1eM?lang=fr";
 
 	return (
     <section className="contact-me" id="smContact">
@@ -12,9 +13,9 @@ const Contact = props => {
         <hr/>
         <p><a href={email} alt='send an email' target="_blank" rel="noopener noreferrer">
           Send an email</a></p>
-        <p><a href={props.twitter} alt="twitter" target="_blank" rel="noopener noreferrer">
+        <p><a href={twitter} alt="twitter" target="_blank" rel="noopener noreferrer">
           DM @S0ph1eM</a></p>
-        <p><a href={props.linkedin} alt="linkedin" target="_blank" rel="noopener noreferrer">
+        <p><a href={linkedin} alt="linkedin" target="_blank" rel="noopener noreferrer">
           Message on Linkedin</a></p>
     </section>
 	)
