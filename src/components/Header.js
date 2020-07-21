adeacd9a308de57ffd7be3ef1068970cf50af44a
S0f1eM <s.marchand56@gmail.com>
2020-07-21 13:46:27 +0200

    lighttheme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import DarkModeToggle from './elements/DarkModeToggle';
-import './../css/DarkModeToggle.css';
 
 const Header = () => {
 
