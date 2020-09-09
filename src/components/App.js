1a73c24e3b93f21e670e77b136e7927d2a726347
S0f1eM <s.marchand56@gmail.com>
2020-09-09 10:21:21 +0200

    add button and mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ScrollTo from './elements/ScrollTo';
+
 
 import Header from './Header';
 import Home from './Home';
@@ -13,15 +15,17 @@ const App = () => {
   return (
     <>
     	<BrowserRouter>
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-				<Route path="/" exact component={Home} />
-        <Route path="/Home" exact component={Home} />
-        <Route path="/Portfolio" exact component={Portfolio} />
-				<Route path="/Resume" exact component={Resume} />
-      </Suspense>
-      <Contact />
-     	<Footer />
+        <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+    				<Route path="/" exact component={Home} />
+            <Route path="/Home" exact component={Home} />
+            <ScrollTo> 
+              <Route path="/Portfolio" exact component={Portfolio} />
+      				<Route path="/Resume" exact component={Resume} />
+            </ScrollTo> 
+          </Suspense>
+        <Contact />
+       	<Footer />
       </BrowserRouter>
     </>
   );
