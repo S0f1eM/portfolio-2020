2091dc83acbba6e6755757573f2ac3c05bd39926
S0f1eM <s.marchand56@gmail.com>
2020-09-20 22:03:17 +0200

    fix little mobile css

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,42 +19,22 @@ const Home = () => {
 
         <ProjectCard 
           title="IUEM Websites" 
-          img="screenshot-iuem.png"
-          skills=" HTML - LESS - PHP - MySQL"
+          skills=" WORDPRESS - PHP - MySQL"
+          content="A university institute and 3 laboratories partners websites."
           url="https://www-iuem.univ-brest.fr"
-        />
-        <ProjectCard  
-                title="Semantic UI Template" 
-                img="screenshot-MoodColors.png"
-                skills=" React - react-semantic-ui"
-                url="http://moodcolors.surge.sh/"
-                github="https://github.com/S0f1eM/neon-template"
-        />
-        <ProjectCard  
-          title="Full CSS Portfolio"
-          img="flexbox-portfolio.png" 
-          skills=" CSS3 - Flexbox - HTLM5 "
-          url="https://flexbox-portfolio.surge.sh"
-          github="https://github.com/S0f1eM/portfolio-full-css"
         />
         <ProjectCard 
                 title="Directory WP Plugin" 
-                img="plugin-directory.png"
-                skills=" PHP - MySQL - JavaScript"
+                skills=" PHP - MySQL - WORDPRESS"
+                content="A WordPress directory manager plugin to manage a directory."                
                 url="https://www-iuem.univ-brest.fr/annuaire/"
                 github="https://github.com/S0f1eM/my-first-wp-plugin"
         />
         <ProjectCard 
                 title="Drum Machine" 
-                img="screenshot-drum-machine.png"
                 skills=" React - React-dom"
+                content="A drum machine build with React & React-dom on Codepen."
                 url="https://codepen.io/s0ph1/full/abNZapq"
-        />
-        <ProjectCard 
-                title="Pictures API Search" 
-                img="screenshot-pictures-search.png"
-                skills=" React - Unsplash API - axios "
-                github="https://github.com/S0f1eM/pics-search-unsplash"
         />
         <Button  link="/Portfolio" 
               label="go to portfolio" 
