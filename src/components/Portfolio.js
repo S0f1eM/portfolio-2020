21831e107ca53a182cf6095c264a610900e30684
S0f1eM <s.marchand56@gmail.com>
2020-09-27 10:48:11 +0200

    test image screen type with adjustments in text (too long)

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,37 +21,37 @@ const Portfolio = () => {
 				    <ProjectCard 
 			          title="Drum Machine" 
 			          img="screenshot-drum-machine.png"
-			          content="A drum machine build with React & React-dom on Codepen for the FreeCodeCamp Front-end projects."
+			          content="A drum machine build with React & React-dom on Codepen."
 			          skills=" React - React-dom"
 			          url="https://codepen.io/s0ph1/full/abNZapq"
 			        />
 			          <ProjectCard 
 			          title="IUEM Websites" 
 			          img="screenshot-iuem.png"
-			          content="A university institute and 3 laboratories ( Lemar, Geosciences and Amure ) partners websites built with WordPress."
+			          content="A university institute and 3 laboratories partners websites."
 			          skills=" HTML - LESS - PHP - MySQL"
 			          url="https://www-iuem.univ-brest.fr"
 			        />
 			        <ProjectCard 
 			          title="Directory WP Plugin" 
-			          img="plugin-directory.png"
-			          content="A WordPress directory manager plugin to manage a researchers directory (php & MySQL) and show the list on the website."
+			          img="screenshot-plugin.png"
+			          content="A WordPress directory plugin to manage and show the directory on the website."
 			          skills=" PHP - MySQL - JavaScript"
 			          url="https://www-iuem.univ-brest.fr/annuaire/"
 			          github="https://github.com/S0f1eM/my-first-wp-plugin"
 			        />
 			        <ProjectCard  
 			          title="Full CSS Portfolio"
-			          img="flexbox-portfolio.png" 
-			          content="A developer portfolio made from scratch with Flexbox and no JavaScript. It was a personal challenge for my 2019 portfolio."
+			          img="screenshot-flexbox.png" 
+			          content="A developer portfolio made from scratch with Flexbox and no JavaScript. "
 			          skills=" CSS3 - Flexbox - HTLM5"
 			          url="https://flexbox-portfolio.surge.sh"
 			          github="https://github.com/S0f1eM/portfolio-full-css"
 			        />
 			        <ProjectCard  
 			          title="Semantic UI Template" 
-			          img="screenshot-MoodColors.png"
-			          content="A Blog template made with Reactjs using the react-semantic-ui integration and shorthands props."
+			          img="screenshot-neon.png"
+			          content="A Blog template made with Reactjs using the react-semantic-ui integration."
 			          skills=" React - react-semantic-ui"
 			          url="http://moodcolors.surge.sh/"
 			          github="https://github.com/S0f1eM/neon-template"
@@ -59,9 +59,9 @@ const Portfolio = () => {
 			         <ProjectCard 
 			          title="Pictures API Search" 
 			          img="screenshot-pictures-search.png"
-			          content="An Images search page made with React and axios client to fetch data using the Unsplash API."
+			          content="CODE ONLY - An Images search page made with React and axios to fetch data."
 			          skills=" React - Unsplash API - axios "
-			          url="https://github.com/S0f1eM/pics-search-unsplash"
+			          github="https://github.com/S0f1eM/pics-search-unsplash"
 			        />	 
 				</div>
 				<Button  link="/About" 
