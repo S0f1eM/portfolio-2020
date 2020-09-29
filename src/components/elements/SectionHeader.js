23655d30e1c9da86855adb019322864704d69d67
S0f1eM <s.marchand56@gmail.com>
2020-09-29 09:50:23 +0200

    css, text etc

--- src/components/elements/SectionHeader.js
+++ src/components/elements/SectionHeader.js
@@ -7,7 +7,7 @@ const SectionHeader = props  => {
     <header className="main-header">
          <div className="pattern small" aria-hidden="true"></div>
           <p className="section-intro" tabindex="0">{props.content}</p>
-          <p className="section-intro mobile">{props.more}</p>
+          <p className="section-intro">{props.more}</p>
         <div className="pattern small" aria-hidden="true"></div>            
     </header>
 	)
